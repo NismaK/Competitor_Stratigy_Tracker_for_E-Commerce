ab0b32288315b4a2ec5addb6d366c3744efc48c7
NismaK <125562510+NismaK@users.noreply.github.com>
2025-03-25 21:57:25 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 def load_competitor_data():
-    return pd.read_csv("Model Building/price_discount_predictions.csv")
+    return pd.read_csv("price_discount_predictions.csv")
 
 def load_reviews_data():
-    return pd.read_csv("Model Building/product_reviews.csv")
+    return pd.read_csv("product_reviews.csv")
 
 def analyze_sentiment(reviews):
     return [{"label": r, "sentiment": "Positive"} for r in reviews]  # Dummy function
